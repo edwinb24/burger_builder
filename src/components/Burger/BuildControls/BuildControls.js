f988577794ef51a66543297a42ba068d932a04b9
edwinb24 <edwin_b24@hotmail.com>
2020-05-02 11:02:40 -0500

    Start making Burger Builder Interactive 🍔

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from "./BuildControls.module.css"
+
+const buildControls = (props) =>
+    <div className={classes.buildControls}>
+
+    </div>    
+
+
+export default buildControls
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,24 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
-    return(
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey =>
+                [...Array(props.ingredients[igKey])].map((_, i) => 
+                    <BurgerIngredient type={igKey} key={igKey+i} />
+                )
+        )
+        .reduce(
+            (arr,el) => arr.concat(el),
+            [])
+
+    if(transformedIngredients.length === 0)  
+        transformedIngredients = <p>Please Start adding Ingredients</p>
+    
+    return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="bread-bottom" />
+            <BurgerIngredient type="bread-top"/>
+                {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
         </div>
     )
 }
