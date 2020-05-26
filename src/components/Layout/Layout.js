02a36625259837cb87a3f00759d01ce0892a460f
edwinb24 <edwin_b24@hotmail.com>
2020-05-26 15:05:41 -0500

    Working on Responsiveness - Side Drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React, {Fragment} from 'react'
 
 import Toolbar from "./../Navigation/Toolbar/Toolbar"
+import SideDrawer from "./../Navigation/SideDrawer/SideDrawer"
 import classes from './Layout.module.css'
 
 const Layout = props => (
     <Fragment>
         <Toolbar />
-        <div>SideDrawer</div>
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
